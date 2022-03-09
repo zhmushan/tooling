e2b891fc4a3a410b3259ec0901b5cd555a22bfe7
木杉 <zhmushan@qq.com>
2022-03-10 00:15:29 +0800

    Make base public path configurable

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { fileURLToPath, URL } from "url";
+import { env } from "process";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const { BasePublicPath = "/" } = env;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tooling/",
+  base: BasePublicPath,
   plugins: [vue()],
   resolve: {
     alias: {
